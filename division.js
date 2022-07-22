23859a7f5d56df7bec38936fa49acc1c583b4479
Vishal Medilinks Pvt Ltd <culbuddy656@gmail.com>
2022-07-22 21:10:15 +0530

    division file added

---
+++ division.js
@@ -0,0 +1,7 @@
+function divide() {
+
+firstNo=document.getElementById("firstno").value
+ secondNo=document.getElementById("secondno").value
+ result=firstNo/secondNo
+ document.getElementById("result").value=result
+}
