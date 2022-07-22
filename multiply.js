b70371f45752ac798e23b75d06985f0df250ac69
shashidhar <shashidhargsg@gmail.com>
2022-07-22 20:53:36 +0530

    multiply file added

---
+++ multiply.js
@@ -0,0 +1,7 @@
+function multiply{
+firstNo=document.getElementById("firstno").value
+ secondNo=document.getElementById("secondno").value
+ result=firstNo*secondNo
+ document.getElementById("result").value=result
+
+}
